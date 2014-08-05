eaec75fc26dffcf03b58413753f6d29e68ed5b07
Mu-An Chiou <me@muanchiou.com>
2014-08-06 00:51:59 +0800

    Positive roadblocking

--- js/skate.js
+++ js/skate.js
@@ -1,29 +1,3 @@
-var Person = function() {
-  this.body = $(".sk8terboi")
-}
-
-var addAndRemoveClass = function(classes, ms) {
-  person.body.toggleClass(classes)
-  setTimeout(function() {
-    person.body.toggleClass(classes)
-  }, ms)
-}
-
-Person.prototype.burst = function() {
-  if(this.body.hasClass("speed")) return false
-  addAndRemoveClass("push", 200)
-  addAndRemoveClass("speed", 4000)
-}
-
-Person.prototype.jump = function() {
-  if(this.isJumping()) return false
-  addAndRemoveClass("ontheground jumping", 200)
-}
-
-Person.prototype.isJumping = function() {
-  return this.body.hasClass("jumping")
-}
-
 $(document).on("keyup", function(event) {
   // space bar
   if(event.keyCode == 32) {
@@ -37,4 +11,9 @@ $(document).on("keyup", function(event) {
 
 $(document).on("ready", function() {
   window.person = new Person()
+  window.track  = new Track()
+
+  setInterval(function() {
+    window.track.addBlock(1)
+  }, 800)
 })
